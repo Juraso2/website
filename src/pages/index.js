f42d4f060d664bb0c9477e12d27f0929e39f8672
Daniel J <danielj@rockon.tech>
2021-11-04 21:44:26 +0100

    Fix: fixed skills ordering

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ export const pageQuery = graphql`
                 }
                 ... on STRAPI_ComponentContentSkillsList {
                   __typename
-                  skills {
+                  skills(sort: "Position:asc") {
                     Title
                     Percent
                   }
