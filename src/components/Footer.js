83edae646d603276132dd1d0767d25a59f01a2de
Daniel J <danielj@rockon.tech>
2021-11-04 18:21:14 +0100

    Fix: footer contact section classes based on clause visibility

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,53 @@
 import * as React from 'react'
+import classNames from "classnames"
 import parsePhoneNumber from "libphonenumber-js"
 
-const Footer = ({ location, phone, email, clause }) => {
+const Footer = ({
+        location,
+        phone,
+        email,
+        clause
+    }) => {
     const phoneNumber = parsePhoneNumber(phone, 'PL')
 
+    let contactClass = classNames(
+        'cv-section_content uk-width-1-1 uk-width-1-2@m uk-padding-large uk-grid-margin uk-first-column', {
+            'uk-padding-remove-bottom': clause
+        }
+    )
+
     return (
-        <section class="uk-section-secondary uk-preserve-color"
+        <section className="uk-section-secondary uk-preserve-color"
             uk-scrollspy="cls: uk-animation-slide-bottom-small; target: .cv-animated-footer; delay: 100; repeat: false">
-            <div class="uk-container uk-container-expand uk-padding-remove">
-                <div class="uk-child-width-expand uk-grid uk-grid-collapse" uk-grid>
-                    <div class="cv-section_header uk-grid-item-match uk-first-column uk-width-1-6 uk-visible@m">
+            <div className="uk-container uk-container-expand uk-padding-remove">
+                <div className="uk-child-width-expand uk-grid uk-grid-collapse" uk-grid>
+                    <div className="cv-section_header uk-grid-item-match uk-first-column uk-width-1-6 uk-visible@m">
                         <div></div>
                     </div>
 
                     <div
-                        class="cv-section_header uk-width-1-1 uk-width-1-5@m uk-padding-large uk-text-right uk-first-column">
-                        <h2 class="cv-section_heading uk-margin-small cv-animated-footer">Kontakt</h2>
+                        className="cv-section_header uk-width-1-1 uk-width-1-5@m uk-padding-large uk-text-right uk-first-column">
+                        <h2 className="cv-section_heading uk-margin-small cv-animated-footer">Kontakt</h2>
                     </div>
 
-                    <div
-                        class="cv-section_content uk-width-1-1 uk-width-1-2@m uk-padding-large uk-grid-margin uk-first-column uk-padding-remove-bottom">
-                        <div class="cv-contact uk-column-1-3@m cv-animated-footer">
+                    <div className={contactClass}>
+                        <div className="cv-contact uk-column-1-3@m cv-animated-footer">
                             {location && (
-                                <div class="cv-contact_block">
+                                <div className="cv-contact_block">
                                     <h4>Lokalizacja</h4>
                                     <address>{location}</address>
                                 </div>
                             )}
 
                             {phoneNumber && (
-                                <div class="cv-contact_block">
+                                <div className="cv-contact_block">
                                     <h4>Telefon</h4>
                                     <p><a href={"tel:" + phone}>{phoneNumber.number}</a></p>
                                 </div>
                             )}
 
                             {email && (
-                                <div class="cv-contact_block">
+                                <div className="cv-contact_block">
                                     <h4>Email</h4>
                                     <a href={"mailto:" + email}>{email}</a>
                                 </div>
@@ -45,17 +56,17 @@ const Footer = ({ location, phone, email, clause }) => {
 
                         {clause && (
                             <>
-                                <hr class="cv-animated-footer" />
+                                <hr className="cv-animated-footer" />
 
                                 <div
-                                    class="cv-clause uk-padding uk-padding-remove-top uk-padding-remove-horizontal cv-animated-footer">
-                                    <p class="cv-clause_content">{clause}</p>
+                                    className="cv-clause uk-padding uk-padding-remove-top uk-padding-remove-horizontal cv-animated-footer">
+                                    <p className="cv-clause_content">{clause}</p>
                                 </div>
                             </>
                         )}
                     </div>
 
-                    <div class="cv-section_content uk-grid-item-match uk-visible@m">
+                    <div className="cv-section_content uk-grid-item-match uk-visible@m">
                         <div></div>
                     </div>
                 </div>
